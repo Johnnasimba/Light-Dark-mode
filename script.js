c3d58c649de6557f1d0afe9dc10e7c3bf481ffc4
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-31 19:44:30 +0200

    Added a comment on top of switchTheme function

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function lightMode() {
   }
   
 
-// 
+//  Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
